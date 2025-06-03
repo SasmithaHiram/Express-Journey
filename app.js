dffb67b9f4f22d8fd760bfb1a543c91be3610520
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-03 17:24:34 +0530

    Post & Get

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+
+app.post("/product/create/:id", (req, res) => {
+  let body = body;
+  let id = req.params.id;
+  res.send(body);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
