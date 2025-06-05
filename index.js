c7da1546ce721fb4b23daafd5edeb5ecfbe501d2
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-05 15:01:01 +0530

    Handling DELETE Requests

--- index.js
+++ index.js
@@ -60,6 +60,17 @@ app.put("/api/course/update/:id", (req, res) => {
   res.send(course);
 });
 
+app.delete("/api/course/delete/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The coursewith the given ID was not found");
+
+  const index = courses.indexOf(course);
+
+  courses.splice(index, 1);
+  res.send(course);
+});
+
 function validateCourse(course) {
   const schema = Joi.object({ name: Joi.string().min(3).required() });
   return schema.validate(course);
