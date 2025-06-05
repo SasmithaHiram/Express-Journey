0794a04979065c9d750f54e7e06820e4d5e61db5
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-05 10:35:08 +0530

    Params

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
+const port = process.env.PORT;
 const express = require("express");
-
 const app = express();
 
 app.get("/", (req, res) => {
@@ -10,6 +11,10 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/api/posts/:year/:month", (req, res) => {
+  res.send(req.params);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
