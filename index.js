e7958a154fba5a9b6eb041176e63108ce5514c14
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-05 12:57:51 +0530

    Input Validation - npm i joi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const port = process.env.PORT;
 const express = require("express");
 const app = express();
 app.use(express.json());
+const Joi = require("joi");
 
 const courses = [
   { id: 1, name: "Sinhala" },
@@ -27,13 +28,22 @@ app.get("/api/course/:id", (req, res) => {
   res.send(course);
 });
 
-app.get("/api/create/course", (req, res) => {
+app.post("/api/course/create", (req, res) => {
+  const schema = Joi.object({ name: Joi.string().min(3).required() });
+  const result = schema.validate(req.body);
+
+  if (result.error) {
+    res.status(404).send(result.error.details[0].message);
+    return;
+  }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
   };
   courses.push(course);
   res.send(course);
+  console.log(course);
 });
 
 app.listen(port, () => {
